dd8f41202d31b886557fd0fe194c55eb6c1ff0ad
Josh Jang <jangkeehyo@gmail.com>
2016-11-18 13:51:15 +0900

    Fetch the data and show it on the push noti
    Fetch the date from the Rest Controller and display it on the
    notification

--- poscoict-license/license-ui/src/main/webapp/sw.js
+++ poscoict-license/license-ui/src/main/webapp/sw.js
@@ -1,18 +1,62 @@
 self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
+//  console.log('Received a push message', event);
 
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = 'dist/img/push.png';
-  var tag = 'simple-push-demo-notification-tag';
+  // var title = 'Yay a message.';
+  // var body = 'We have received a push message.';
+  // var icon = 'dist/img/push.png';
+  // var tag = 'simple-push-demo-notification-tag';
 
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
+  event.waitUntil(fetch("/solutionpot/greeting").then(function(response) {
+        if (response.status !== 200) {
+          // Either show a message to the user explaining the error
+          // or enter a generic message and handle the
+          // onnotificationclick event to direct the user to a web page
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          throw new Error();
+        }
+
+        // Examine the text in the response
+        return response.json().then(function(data) {
+          console.log("response data : ", data);
+          if (!data === undefined) {
+            console.error('The API returned an error.', data.error);
+            throw new Error();
+          }
+
+          var title = 'Yay another message.';
+          var body = data.content;
+          var icon = 'dist/img/push.png';
+          var tag = 'simple-push-demo-notification-tag';
+
+          return self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+          });
+        });
+      }).catch(function(err) {
+        console.error('Unable to retrieve data', err);
+
+        var title = 'An error occurred';
+        var body = 'We were unable to get the information for this push message';
+        var icon = 'dist/img/push.png';
+        var notificationTag = 'notification-error';
+        return self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: notificationTag
+          });
+      })
+    );
+
+  // original
+  // event.waitUntil(
+  //   self.registration.showNotification(title, {
+  //     body: body,
+  //     icon: icon,
+  //     tag: tag
+  //   })
+  // );
 });
 
 self.addEventListener('notificationclick', function(event) {
